f62a35d2eeab0deb5a3e88e428ccaae641370d28
tom tomsen <tom.tomsen@mailbox.org>
2020-11-16 23:41:53 +0100

    Remove @actions/github
    github payload is not required

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
 	console.log('Hello world');
 	core.setOutput('output', 'my output');
-
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`payload ${payload}`);
 } catch (error) {
 	core.setFailed(error.message);
 }
